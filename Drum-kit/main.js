e36672eb07a92aa0dd9c59104ebcc9812465bc1b
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-10-05 22:48:32 +0530

    Drum-Kit Project

---
+++ Drum-kit/main.js
@@ -0,0 +1,47 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInner = this.innerHTML;
+    makeSound(buttonInner);
+  });
+}
+
+document.addEventListener("keypress", function (event) {
+  makeSound(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "k":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "l":
+      var kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+    
+    default: console.log(key);
+  }
+}
